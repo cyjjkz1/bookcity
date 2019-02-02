bf6a251bdd4133b3be06c1d0c4b60e35b19c8507
cyjjkz1 <cyjjkz1@sina.com>
2019-02-02 14:26:33 +0800

    catch mysql error

--- app/v1/handlers/category_handler.py
+++ app/v1/handlers/category_handler.py
@@ -162,7 +162,7 @@ class AgeGroupAddFuncHandler(BaseHandler):
                                            params['func_id']))
             app.logger.info('<Function>DB query result: {}'.format(func.model_to_dict(query_relation=False)))
 
-            age.functions = [func]
+            age.functions.append(func)
             age.save()
             return self.request_finish(RESP_CODE.SUCCESS, RESP_ERR_MSG.get(RESP_CODE.SUCCESS, ''))
         except BaseException as e:
